ef8418004d79b1f0f0a09327022f2fe197e360f1
Sandy Nascimento <sandymnascimento@outlook.com>
2023-10-24 21:29:43 -0300

    wiki on going

--- day-03/ex00/geohashing.py
+++ day-03/ex00/geohashing.py
@@ -4,7 +4,7 @@ import antigravity
 def geohashing(latitude, longitude, date="2005-05-26-10458.68"):
   print(antigravity.geohash(latitude, longitude, date.encode()))
 
-if __name__ == '__main__':    
+def main():
     try:
         if len(sys.argv) == 2:
             geohashing(float(sys.argv[1]), float(sys.argv[2]))
@@ -13,5 +13,7 @@ if __name__ == '__main__':
     except ValueError as e:
         print(e)
         sys.exit(1)
+if __name__ == '__main__':    
+    main()
 
 #37.421542 -122.085589

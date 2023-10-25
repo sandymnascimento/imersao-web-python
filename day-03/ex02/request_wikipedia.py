79f11f6cf41f1db9e84930283703493939c5ae3f
Sandy Nascimento <sandymnascimento@outlook.com>
2023-10-25 17:29:52 -0300

    testes e tentativas.

--- day-03/ex02/request_wikipedia.py
+++ day-03/ex02/request_wikipedia.py
@@ -1,30 +1,59 @@
+#!/usr/bin/env python3
+ 
 import sys
 import json
 import requests
 import dewiki
 
+import wikipedia 
+
 def search_wiki(busca):
-    url = "https://fr.wikipedia.org/w/api.php"
+    url = "https://en.wikipedia.org/w/api.php"
+    '''
     parametros = {
         "action": "query",
         "format": "json",
-        "list": "search",
+        'titles': busca,
         "prop": "extracts",
-        "srsearch": busca
+        "exintro": True,
+        "plaintext": True
+    }
+    '''
+
+    parametros = {
+        "action": "query",
+        "format": "json",
+        'titles': busca,
+        "prop": "extracts",
+        "exintro": True,
+        "plaintext": True
     }
 
     response = requests.get(url=url, params=parametros)
 
     if response.status_code != 200:
         return None
+    
+    print(response)
 
-    response_json = json.loads(response.content)
-    print(response_json)
+    data = response.json()
+    
+
+    page = next(iter(data['query']['pages'].values()))
+    
+    print(page['extract'].replace('</p><p>', '\n'))
+    x = busca.replace(' ', '_')
+    result= open(f'{x}.html', 'w')
+    result.write(page['extract'])
+
+    '''
+    
     article_extract = response_json["query"]["pages"][0]["extract"]
 
     article_text = dewiki.remove_wiki_markup(article_extract)
 
     print(article_text)
+'''
 
 def main():
     #adicionar opção de escolher o idioma
@@ -33,3 +62,28 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+     '''
+    @property
+  def content(self):
+    '''
+ #   Plain text content of the page, excluding images, tables, and other data.
+    '''
+
+    if not getattr(self, '_content', False):
+      query_params = {
+        'prop': 'extracts|revisions',
+        'explaintext': '',
+        'rvprop': 'ids'
+      }
+      if not getattr(self, 'title', None) is None:
+         query_params['titles'] = self.title
+      else:
+         query_params['pageids'] = self.pageid
+      request = _wiki_request(query_params)
+      self._content     = request['query']['pages'][self.pageid]['extract']
+      self._revision_id = request['query']['pages'][self.pageid]['revisions'][0]['revid']
+      self._parent_id   = request['query']['pages'][self.pageid]['revisions'][0]['parentid']
+
+    return self._content
+     '''

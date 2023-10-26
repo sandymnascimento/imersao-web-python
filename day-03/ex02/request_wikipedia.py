ff93a92b4f49a61dd386ff58254dc8a9b699026f
Sandy Nascimento <sandymnascimento@outlook.com>
2023-10-26 11:00:53 -0300

    funcionando ?

--- day-03/ex02/request_wikipedia.py
+++ day-03/ex02/request_wikipedia.py
@@ -1,89 +1,68 @@
-#!/usr/bin/env python3
- 
 import sys
 import json
-import requests
 import dewiki
+import requests
 
-import wikipedia 
+def _wiki_request(params):
+    params['format'] = 'json'
+    if not 'action' in params:
+        params['action'] = 'query'
 
-def search_wiki(busca):
-    url = "https://en.wikipedia.org/w/api.php"
-    '''
-    parametros = {
-        "action": "query",
-        "format": "json",
-        'titles': busca,
-        "prop": "extracts",
-        "exintro": True,
-        "plaintext": True
+    r = requests.get('http://en.wikipedia.org/w/api.php', params=params)
+
+    return r.json()
+
+def search(query, results=10, suggestion=False):
+    search_params = {
+        'list': 'search',
+        'srprop': '',
+        'srlimit': results,
+        'limit': results,
+        'srsearch': query
     }
-    '''
+    if suggestion:
+        search_params['srinfo'] = 'suggestion'
 
-    parametros = {
-        "action": "query",
-        "format": "json",
-        'titles': busca,
-        "prop": "extracts",
-        "exintro": True,
-        "plaintext": True
-    }
+    raw_results = _wiki_request(search_params)
 
-    response = requests.get(url=url, params=parametros)
+    search_results = (d['title'] for d in raw_results['query']['search'])
 
-    if response.status_code != 200:
-        return None
-    
-    print(response)
+    if suggestion:
+        if raw_results['query'].get('searchinfo'):
+            return list(search_results), raw_results['query']['searchinfo']['suggestion']
+        else:
+            return list(search_results), None
 
-    data = response.json()
-    
+    return list(search_results)
 
-    page = next(iter(data['query']['pages'].values()))
-    
-    print(page['extract'].replace('</p><p>', '\n'))
-    x = busca.replace(' ', '_')
-    result= open(f'{x}.html', 'w')
-    result.write(page['extract'])
+def page(title=None, auto_suggest=True):
+    if title is not None:
+        if auto_suggest:
+            results, suggestion = search(title, results=1, suggestion=True)
+            title = suggestion or results[0]
+        query_params = {
+            'prop': 'extracts|revisions',
+            'explaintext': '',
+            'rvprop': 'ids'
+        }
+        query_params['titles'] = title
 
-    '''
-    
-    article_extract = response_json["query"]["pages"][0]["extract"]
+        request = _wiki_request(query_params)
+        query = request['query']
+        pageid = list(query['pages'].keys())[0]
 
-    article_text = dewiki.remove_wiki_markup(article_extract)
+        x = title.replace(' ', '_')
+        result= open(f'{x}.wiki', 'w')
+        result.write(request['query']['pages'][pageid]['extract'])
 
-    print(article_text)
-'''
 
 def main():
     #adicionar opção de escolher o idioma
     if len(sys.argv) == 2:
-        search_wiki(sys.argv[1])
+        page(sys.argv[1])
 
 if __name__ == '__main__':
     main()
 
-     '''
-    @property
-  def content(self):
-    '''
- #   Plain text content of the page, excluding images, tables, and other data.
-    '''
 
-    if not getattr(self, '_content', False):
-      query_params = {
-        'prop': 'extracts|revisions',
-        'explaintext': '',
-        'rvprop': 'ids'
-      }
-      if not getattr(self, 'title', None) is None:
-         query_params['titles'] = self.title
-      else:
-         query_params['pageids'] = self.pageid
-      request = _wiki_request(query_params)
-      self._content     = request['query']['pages'][self.pageid]['extract']
-      self._revision_id = request['query']['pages'][self.pageid]['revisions'][0]['revid']
-      self._parent_id   = request['query']['pages'][self.pageid]['revisions'][0]['parentid']
-
-    return self._content
-     '''
+        
